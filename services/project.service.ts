3c0ba1d3086bf9af6701c28f711847ac6c8c794c
Gustavo Araujo <gustavoaraujo.dev@gmail.com>
2022-10-13 22:45:08 -0300

    feat(endpoints): create endpoint to get Task and Project

--- services/project.service.ts
+++ services/project.service.ts
@@ -1,4 +1,4 @@
-import { Service, ServiceBroker, Context } from 'moleculer';
+import { Service, ServiceBroker, Errors, Context } from 'moleculer';
 import { Project } from '../data';
 import { IProject } from '../models/project';
 
@@ -14,14 +14,35 @@ export default class ProjectService extends Service {
                         method: 'GET',
                         path: '/'
                     },
-                    async handler(): Promise<IProject> {
+                    async handler(): Promise<IProject[]> {
                         return await this.getProjects();
-                    }
-                }
+                    },
+                },
+                getProjectById: {
+                    rest: {
+                        method: 'GET',
+                        path: '/:id',
+                    },
+                    params: {
+                        id: 'string',
+                    },
+                    /** @param {Context} ctx */
+                    async handler(ctx): Promise<IProject> {
+                        return await this.getProjectById(ctx.params?.id);
+                    },
+                },
             },
             methods: {
                 async getProjects(): Promise<IProject[]> {
                     return await Project.query();
+                },
+                async getProjectById(projectId: string): Promise<IProject> {
+                    const project = await Project.query().findById(projectId);
+
+                    if (!project) {
+                        throw new Errors.MoleculerClientError('Project not found', 404);
+                    }
+                    return project;
                 }
             }
         })
